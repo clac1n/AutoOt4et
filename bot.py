7016eef89daad3c11c1135239c614f33f69c6b0a
root <root@2817661-iz52691.twc1.net>
2024-05-02 15:38:30 +0300

    v1.1

---
+++ bot.py
@@ -0,0 +1,26 @@
+import telebot
+from script import server, get_report_text
+
+# Токен бота
+bot_token = '7097731089:AAFJdPIk82wglZQWsSwZ4iU_r6tc_iGNsZQ'
+
+# Создание экземпляра бота
+bot = telebot.TeleBot(bot_token)
+
+# Обработчик команды /report_start
+@bot.message_handler(commands=['report_start'])
+def start_report(message):
+  # Поиск непрочитанных писем
+  result, ids = server.search(None, '(UNSEEN)')
+
+  # Обработка непрочитанных писем
+  for id in ids[0].split():
+    # Получение информации из письма
+    report_text = get_report_text(id)
+
+    # Отправка отчета в Telegram
+    bot.send_message(message.chat.id, report_text)
+
+# Запуск бота
+bot.polling()
+
